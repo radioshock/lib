d6b37282b170faf545fa22cc1a5a0f4e1b315fe6
DINGUS <89667169+shortmode@users.noreply.github.com>
2021-12-24 15:00:00 -0800

    Add files via upload

--- altio.h
+++ altio.h
@@ -1,6 +1,6 @@
 /*  ALTIO.H
-    C alternate standard library
-    Standard Input/Output
+	C alternate standard library
+	Standard Input/Output
 */
 
 #if !defined(_ALTIO_H)
@@ -8,9 +8,11 @@
 
 #if defined(__unix__) || defined(__APPLE__)
 #include "unistd.h"
-#else
-#error "altio.h: Non-unix platforms are unsupported."
-#endif // __unix__ __APPLE__
+#elif defined(_WIN32)
+#error "altio.h: Microsoft Windows is not supported at this time."
+#elif defined(__ACRYLIC__)
+#include "interface.h"
+#endif // __unix__ __APPLE__ _WIN32 __ACRYLIC__
 
 #include "altarg.h"
 #include "altdef.h"
@@ -23,18 +25,18 @@
 // Screen input/output
 
 int putchar(int c) {
-    char out = c;
-    write(_altout, &out, sizeof(out));
-    return 1;
+	char out = c;
+	write(_altout, &out, sizeof(out));
+	return 1;
 }
 
 #define print(c,s) for(size_t i = 0; i < (size_t)s; i++) putchar( ((const char*)c) [i]);
 
 int printf(const char* restrict format, ...) {
-    va_list parameters;
-    va_start(parameters, format);
-    for(size_t i=0; i < strlen(format); i++) {
-        if(format[i] == '%') {
+	va_list parameters;
+	va_start(parameters, format);
+	for(size_t i=0; i < strlen(format); i++) {
+		if(format[i] == '%') {
 			i++;
 			if(format[i] == 'c') {
 				char c = (char)va_arg(parameters, int);
@@ -43,52 +45,52 @@ int printf(const char* restrict format, ...) {
 			else if(format[i] == 's') {
 				const char* c = va_arg(parameters, const char*);
 				print(c, strlen(c));
-            }
-            else if(format[i] == 'i') {
-                const char* c = itoa(va_arg(parameters, int), 10);
-                print(c, strlen(c));
-            }
+			}
+			else if(format[i] == 'i') {
+				const char* c = itoa(va_arg(parameters, int), 10);
+				print(c, strlen(c));
+			}
 		}
 		else putchar(format[i]);
-    }
-    va_end(parameters);
-    return 1;
+	}
+	va_end(parameters);
+	return 1;
 }
 
 // Keyboard Input/Output
 
 const char* scan() {
-    static char in[255];
-    read(_altin, in, 255);
-    return in;
+	static char in[255];
+	read(_altin, in, 255);
+	return in;
 }
 
 int getchar(void) {
-    const char* in;
-    in = scan();
-    return in[0];
+	const char* in;
+	in = scan();
+	return in[0];
 }
 
 int scanf(const char* format, ...) {
-    va_list parameters;
-    va_start(parameters, format);
-    for(size_t i=0; i < strlen(format); i++) {
-        if(format[i] == '%') {
+	va_list parameters;
+	va_start(parameters, format);
+	for(size_t i=0; i < strlen(format); i++) {
+		if(format[i] == '%') {
 			i++;
 			if(format[i] == 'c') {
 				char* buffer = va_arg(parameters, char*);
-                char c = getchar();
-                *buffer = c;
+				char c = getchar();
+				*buffer = c;
 			}
 			else if(format[i] == 's') {
 				char** buffer = va_arg(parameters, char**);
-                char* c = (char*)scan();
-                *buffer = c;
-            }
-        }
-    }
-    va_end(parameters);
-    return 1;
+				char* c = (char*)scan();
+				*buffer = c;
+			}
+		}
+	}
+	va_end(parameters);
+	return 1;
 }
 
 #endif // _ALTIO_H
